33120667a3fd83329bea380b0d528defe44e862f
PauloMats <pm.mats98@gmail.com>
2025-07-17 15:02:08 -0300

    feat(tecnologias-page): add basic page structure

---
+++ src/app/tecnologias/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function TecnologiasPage() {
+    return (
+        <main>
+            <h1>Tecnologias</h1>
+            <p>Bem-vindo à página de tecnologias!</p>
+        </main>
+    );
+}
