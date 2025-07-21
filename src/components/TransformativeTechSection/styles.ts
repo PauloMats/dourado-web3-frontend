31fa03f66a63f044e7c91a83a08eda3343693455
PauloMats <pm.mats98@gmail.com>
2025-07-21 15:49:14 -0300

    feat(assets): Add background image to TechnologyHeroSection

--- src/components/TransformativeTechSection/styles.ts
+++ src/components/TransformativeTechSection/styles.ts
@@ -4,23 +4,12 @@ import { colors } from '../../assets/scss/colors';
 
 export const SectionWrapper = styled.section`
 
-  background-image: url('/img/background-dourado.png');
+  background-image: url('/img/background-dourado2.png');
   background-color: #111;
   background-size: cover;
   background-position: center;
   padding: 8rem 0;
   position: relative;
-  
-   // Overlay escuro opcional para legibilidade
-  &::before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: rgba(0, 0, 0, 0.6);
-  }
 `;
 
 export const LinkButton = styled.a`
